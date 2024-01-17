e81eacad8a30c5380d0a1ffcc4b6ffdd266f3fed
atavada <ardhaardhanaputra@gmail.com>
2024-01-17 09:42:22 +0700

    feat: add sign-in and sign-up pages

---
+++ app/(auth)/sign-in/page.jsx
@@ -0,0 +1,14 @@
+export default function SignIn() {
+	return (
+		<>
+			<div className='flex'>
+				<div className='bg-white justify-center items-center h-screen w-screen px-32 py-24'>
+					<div className='bg-[#1EA896] rounded-xl shadow-xl h-full w-full text-center justify-center items-center'>
+						form login
+					</div>
+				</div>
+				<div className='bg-[#f9e7e7] h-screen w-screen' />
+			</div>
+		</>
+	);
+}
---
+++ app/(auth)/sign-up/page.jsx
@@ -0,0 +1,14 @@
+export default function SignUp() {
+	return (
+		<>
+			<div className='flex'>
+				<div className='bg-[#f9e7e7] h-screen w-screen' />
+				<div className='bg-white justify-center items-center h-screen w-screen px-32 py-24'>
+					<div className='bg-[#1EA896] rounded-xl shadow-xl h-full w-full text-center justify-center items-center'>
+						form signup
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}

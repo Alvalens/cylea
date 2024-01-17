046cc966a66cbe3159e38c64c265f7ec0fbcb879
atavada <ardhaardhanaputra@gmail.com>
2024-01-17 09:43:33 +0700

    refactor: user layout component

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -1,14 +1,12 @@
-export default function Layout({ children }) {
+export default function UserLayout({ children }) {
 	return (
 		<>
-			<div className='py-24 w-full rounded-b-3xl bg-[#1EA896] '>
+			<div className='py-24 w-full rounded-b-3xl bg-[#1EA896] absolute'>
 				<div className='text-center text-9xl font-extrabold tracking-[1em] pl-[1em] text-white opacity-30'>
 					FISIKA
 				</div>
 			</div>
-			<div className='my-12'>
-				<main>{children}</main>
-			</div>
+			<div className='pb-16 pt-96 bg-white'>{children}</div>
 		</>
 	);
 }

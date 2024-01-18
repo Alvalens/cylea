302c97c4062a05bd4c4fe68d63d27499511df1c9
Alvalen <alvalen.shafel04@gmail.com>
2024-01-18 20:53:26 +0700

    feat: changes rendered materials

--- app/(user)/material/page.jsx
+++ app/(user)/material/page.jsx
@@ -54,59 +54,69 @@ export default function Page() {
 
 	return (
 		<>
-			<div className='flex'>
-				<div className='container mx-auto px-24'>
-					<div className='border-2 rounded-2xl py-4 bg-[#011627]'>
-						<div className='text-center font-semibold uppercase text-3xl mx-52 text-white'>
+			<div className="flex">
+				<div className="container mx-auto px-24">
+					<div className="border-2 rounded-2xl py-4 bg-[#011627]">
+						<div className="text-center font-semibold uppercase text-3xl mx-52 text-white">
 							Gaya
 						</div>
 					</div>
-					<div className='mt-12 text-[#1EA896] text-3xl font-bold'>
+					<div className="mt-12 text-[#1EA896] text-3xl font-bold">
 						Apa itu gaya?
 					</div>
-					<div className='mt-4 text-lg '>
+					<div className="mt-4 text-lg ">
 						{loading ? (
 							<p>Loading...</p>
 						) : (
-							<div className='text-justify text-xl'>
+							<div className="text-justify text-xl">
 								{response ? (
 									<>
-										{response.judul && <h1>{response.judul}</h1>}
-										{response.materi &&
-											response.materi.map((item, index) => (
-												<p
-													key={index}
-													className='mb-3 justify-start text-justify'
-													dangerouslySetInnerHTML={{
-														__html: item,
-													}}
-												/>
-											))}
+										{response.title && (
+											<h1>{response.title}</h1>
+										)}
+										{response.content &&
+											response.materi.map(
+												(item, index) => (
+													<p
+														key={index}
+														className="mb-3 justify-start text-justify"
+														dangerouslySetInnerHTML={{
+															__html: item,
+														}}
+													/>
+												)
+											)}
 									</>
 								) : (
 									<>
-										Gaya pada Fisika Gaya adalah salah satu konsep dasar dalam
-										fisika. Gaya didefinisikan sebagai interaksi apa pun yang
-										dapat menyebabkan sebuah benda bermassa mengalami perubahan
-										gerak, baik dalam bentuk arah, maupun konstruksi geometris.
-										Dengan kata lain, sebuah gaya dapat menyebabkan sebuah objek
-										dengan massa tertentu mengalami perubahan kecepatan.
-										Perubahan kecepatan dapat terjadi dari kondisi benda diam
-										menjadi bergerak, kondisi benda yang mengalami pertambahan
-										kecepatan (berakselerasi), maupun mengalami perlambatan
-										kecepatan.
+										Gaya pada Fisika Gaya adalah salah satu
+										konsep dasar dalam fisika. Gaya
+										didefinisikan sebagai interaksi apa pun
+										yang dapat menyebabkan sebuah benda
+										bermassa mengalami perubahan gerak, baik
+										dalam bentuk arah, maupun konstruksi
+										geometris. Dengan kata lain, sebuah gaya
+										dapat menyebabkan sebuah objek dengan
+										massa tertentu mengalami perubahan
+										kecepatan. Perubahan kecepatan dapat
+										terjadi dari kondisi benda diam menjadi
+										bergerak, kondisi benda yang mengalami
+										pertambahan kecepatan (berakselerasi),
+										maupun mengalami perlambatan kecepatan.
 										<Image
-											className='my-8 mx-auto'
-											src='/dummy_1.png'
-											alt='Cylea Long Logo'
+											className="my-8 mx-auto"
+											src="/dummy_1.png"
+											alt="Cylea Long Logo"
 											width={250}
 											height={0}
 										/>
-										Gaya memiliki besaran (magnitude) dan arah sehingga termasuk
-										dalam besaran vektor. Besaran gaya diukur dalam satuan
-										Newton (N), sedangkan arah gaya ditentukan oleh arah garis
-										gaya. Garis gaya adalah garis yang menunjukkan arah gaya
-										yang bekerja pada suatu benda.
+										Gaya memiliki besaran (magnitude) dan
+										arah sehingga termasuk dalam besaran
+										vektor. Besaran gaya diukur dalam satuan
+										Newton (N), sedangkan arah gaya
+										ditentukan oleh arah garis gaya. Garis
+										gaya adalah garis yang menunjukkan arah
+										gaya yang bekerja pada suatu benda.
 									</>
 								)}
 							</div>
@@ -115,43 +125,44 @@ export default function Page() {
 				</div>
 
 				{/* sidebar */}
-				<div className='bg-gray-100 rounded-l-3xl w-96 h-auto p-6'>
-					<button className='text-white bg-black rounded-3xl w-12 h-12 inline-flex justify-center items-center mb-4'>
+				<div className="bg-gray-100 rounded-l-3xl w-96 h-auto p-6">
+					<button className="text-white bg-black rounded-3xl w-12 h-12 inline-flex justify-center items-center mb-4">
 						<ChevronRight />
 					</button>
 					<Accordion open={alwaysOpen}>
 						<AccordionHeader
-							className='justify-start'
-							onClick={handleAlwaysOpen}
-						>
-							<ChevronUp className='mr-2' />
+							className="justify-start"
+							onClick={handleAlwaysOpen}>
+							<ChevronUp className="mr-2" />
 							Gaya
-							<div className='ml-auto text-sm'>
-								0%&nbsp;selesai<span className='font-medium ml-1'>1/5</span>
+							<div className="ml-auto text-sm">
+								0%&nbsp;selesai
+								<span className="font-medium ml-1">1/5</span>
 							</div>
 						</AccordionHeader>
 						<AccordionBody>
-							<div className='flex'>
-								<div className='border border-black w-0 opacity-10' />
-								<div className='flex flex-col gap-4'>
-									<div className='inline-flex items-center font-semibold text-base text-black'>
-										<Circle className='ml-4 mr-2 h-4 w-4 ' /> Apa itu Gaya?
+							<div className="flex">
+								<div className="border border-black w-0 opacity-10" />
+								<div className="flex flex-col gap-4">
+									<div className="inline-flex items-center font-semibold text-base text-black">
+										<Circle className="ml-4 mr-2 h-4 w-4 " />{" "}
+										Apa itu Gaya?
 									</div>
-									<div className='inline-flex items-center font-medium text-base'>
-										<Circle className='ml-4 mr-2 h-4 w-4' /> Penyebab Timbulnya
-										Gaya
+									<div className="inline-flex items-center font-medium text-base">
+										<Circle className="ml-4 mr-2 h-4 w-4" />{" "}
+										Penyebab Timbulnya Gaya
 									</div>
-									<div className='inline-flex items-center font-medium text-base'>
-										<Circle className='ml-4 mr-2 h-4 w-4' /> Peran Pentingnya
-										Gaya
+									<div className="inline-flex items-center font-medium text-base">
+										<Circle className="ml-4 mr-2 h-4 w-4" />{" "}
+										Peran Pentingnya Gaya
 									</div>
-									<div className='inline-flex items-center font-medium text-base'>
-										<Circle className='ml-4 mr-2 h-4 w-4' /> Contoh Gaya di
-										Sekitar Kita
+									<div className="inline-flex items-center font-medium text-base">
+										<Circle className="ml-4 mr-2 h-4 w-4" />{" "}
+										Contoh Gaya di Sekitar Kita
 									</div>
-									<div className='inline-flex items-center font-medium text-base'>
-										<Circle className='ml-4 mr-2 h-4 w-4' /> Uji Kemampuan :
-										Gaya
+									<div className="inline-flex items-center font-medium text-base">
+										<Circle className="ml-4 mr-2 h-4 w-4" />{" "}
+										Uji Kemampuan : Gaya
 									</div>
 								</div>
 							</div>
@@ -159,9 +170,8 @@ export default function Page() {
 					</Accordion>
 					<Accordion open={open === 1}>
 						<AccordionHeader
-							className='justify-start'
-							onClick={() => handleOpen(1)}
-						>
+							className="justify-start"
+							onClick={() => handleOpen(1)}>
 							<ChevronDown
 								className={`${
 									1 === open ? "-rotate-180" : ""
@@ -170,14 +180,13 @@ export default function Page() {
 							Usaha
 						</AccordionHeader>
 						<AccordionBody>
-							<div className='border border-black h-12 w-0 opacity-10' />
+							<div className="border border-black h-12 w-0 opacity-10" />
 						</AccordionBody>
 					</Accordion>
 					<Accordion open={open === 2}>
 						<AccordionHeader
-							className='justify-start'
-							onClick={() => handleOpen(2)}
-						>
+							className="justify-start"
+							onClick={() => handleOpen(2)}>
 							<ChevronDown
 								className={`${
 									2 === open ? "-rotate-180" : ""
@@ -186,14 +195,13 @@ export default function Page() {
 							Energi
 						</AccordionHeader>
 						<AccordionBody>
-							<div className='border border-black h-12 w-0 opacity-10' />
+							<div className="border border-black h-12 w-0 opacity-10" />
 						</AccordionBody>
 					</Accordion>
 					<Accordion open={open === 3}>
 						<AccordionHeader
-							className='justify-start'
-							onClick={() => handleOpen(3)}
-						>
+							className="justify-start"
+							onClick={() => handleOpen(3)}>
 							<ChevronDown
 								className={`${
 									3 === open ? "-rotate-180" : ""
@@ -202,14 +210,13 @@ export default function Page() {
 							Kekekalan Energi
 						</AccordionHeader>
 						<AccordionBody>
-							<div className='border border-black h-12 w-0 opacity-10' />
+							<div className="border border-black h-12 w-0 opacity-10" />
 						</AccordionBody>
 					</Accordion>
 					<Accordion open={open === 4}>
 						<AccordionHeader
-							className='justify-start'
-							onClick={() => handleOpen(4)}
-						>
+							className="justify-start"
+							onClick={() => handleOpen(4)}>
 							<ChevronDown
 								className={`${
 									4 === open ? "-rotate-180" : ""
@@ -218,43 +225,43 @@ export default function Page() {
 							Termodinamika
 						</AccordionHeader>
 						<AccordionBody>
-							<div className='border border-black h-12 w-0 opacity-10' />
+							<div className="border border-black h-12 w-0 opacity-10" />
 						</AccordionBody>
 					</Accordion>
 				</div>
 			</div>
 
-			<div className='mt-16 mx-auto space-y-4 max-w-80'>
+			<div className="mt-16 mx-auto space-y-4 max-w-80">
 				<button
-					type='submit'
+					type="submit"
 					onClick={handleSubmit}
-					className='uppercase border-2 border-black rounded-xl py-4 shadow-lg shadow-gray-400 bg-[#BBFF29] w-80'
-				>
-					<p className='text-center font-bold'>sederhanakan?</p>
+					className="uppercase border-2 border-black rounded-xl py-4 shadow-lg shadow-gray-400 bg-[#BBFF29] w-80">
+					<p className="text-center font-bold">sederhanakan?</p>
 				</button>
-				<div className='flex space-x-2'>
+				<div className="flex space-x-2">
 					<button
 						disabled
-						className='inline-flex justify-center items-center rounded-lg bg-[#BBFF29] shadow-md shadow-gray-400 w-10 h-10 border-2 border-black disabled:opacity-30'
-					>
+						className="inline-flex justify-center items-center rounded-lg bg-[#BBFF29] shadow-md shadow-gray-400 w-10 h-10 border-2 border-black disabled:opacity-30">
 						<ChevronLeft />
 					</button>
-					<button className='rounded-lg bg-[#011627] shadow-md shadow-gray-400 w-10 h-10'>
-						<p className='text-white font-semibold text-center'>1</p>
+					<button className="rounded-lg bg-[#011627] shadow-md shadow-gray-400 w-10 h-10">
+						<p className="text-white font-semibold text-center">
+							1
+						</p>
 					</button>
-					<button className='rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10'>
-						<p className='font-semibold text-center'>2</p>
+					<button className="rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10">
+						<p className="font-semibold text-center">2</p>
 					</button>
-					<button className='rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10'>
-						<p className='font-semibold text-center'>3</p>
+					<button className="rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10">
+						<p className="font-semibold text-center">3</p>
 					</button>
-					<button className='rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10'>
-						<p className='font-semibold text-center'>4</p>
+					<button className="rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10">
+						<p className="font-semibold text-center">4</p>
 					</button>
-					<button className='rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10'>
-						<p className='font-semibold text-center'>5</p>
+					<button className="rounded-lg bg-[#DED6D6] shadow-md shadow-gray-400 w-10 h-10">
+						<p className="font-semibold text-center">5</p>
 					</button>
-					<button className='inline-flex justify-center items-center rounded-lg bg-[#BBFF29] shadow-md shadow-gray-400 w-10 h-10 border-2 border-black'>
+					<button className="inline-flex justify-center items-center rounded-lg bg-[#BBFF29] shadow-md shadow-gray-400 w-10 h-10 border-2 border-black">
 						<ChevronRight />
 					</button>
 				</div>
